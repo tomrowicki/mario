fc0f5fd972a8fa841f1c6558b76afea40b588a1f
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-08-07 14:24:37 +0200

    Beginning Scene Hierarchy & ImGui Upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ tasks.test {
 val lwjglVersion = "3.2.3"
 val jomlVersion = "1.10.6"
 val lwjglNatives = "natives-windows"
-val imguiVersion = "1.77-0.17.1"
+val imguiVersion = "1.81.0"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -42,12 +42,13 @@ dependencies {
     implementation("org.joml", "joml", jomlVersion)
 
     // ImGUI stuff
-    implementation ("io.imgui.java:binding:$imguiVersion")
-    implementation ("io.imgui.java:lwjgl3:$imguiVersion")
+    implementation("io.github.spair:imgui-java-app:$imguiVersion")
+//    implementation ("io.imgui.java:binding:$imguiVersion")
+//    implementation ("io.imgui.java:lwjgl3:$imguiVersion")
 
     // Include all available natives, but it's likely that you want something specific
-    runtimeOnly ("io.imgui.java:natives-windows:$imguiVersion")
-    runtimeOnly ("io.imgui.java:natives-windows-x86:$imguiVersion")
+//    runtimeOnly ("io.imgui.java:natives-windows:$imguiVersion")
+//    runtimeOnly ("io.imgui.java:natives-windows-x86:$imguiVersion")
 
     implementation ("com.google.code.gson:gson:2.11.0")
 
