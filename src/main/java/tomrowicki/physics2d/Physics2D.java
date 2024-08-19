f836bfba96f65c8a4e49b8eed6c1965840d557f2
Tomasz Mrowicki <tomrowicki@gmail.com>
2024-08-19 11:55:33 +0200

    Goomba AI

--- src/main/java/tomrowicki/physics2d/Physics2D.java
+++ src/main/java/tomrowicki/physics2d/Physics2D.java
@@ -5,12 +5,16 @@ import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.*;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
+import tomrowicki.components.Ground;
+import tomrowicki.engine.Window;
 import tomrowicki.physics2d.components.PillboxCollider;
 import tomrowicki.engine.GameObject;
 import tomrowicki.engine.Transform;
 import tomrowicki.physics2d.components.Box2DCollider;
 import tomrowicki.physics2d.components.CircleCollider;
 import tomrowicki.physics2d.components.Rigidbody2D;
+import tomrowicki.renderer.DebugDraw;
 
 public class Physics2D {
     private Vec2 gravity = new Vec2(0, -10.0f);
@@ -216,5 +220,26 @@ public class Physics2D {
 
     public boolean isLocked() {
         return world.isLocked();
+    }
+
+    public static boolean checkOnGround(
+            GameObject gameObject,
+            float innerPlayerWidth,
+            float height) {
+        Vector2f raycastBegin = new Vector2f(gameObject.transform.position);
+        raycastBegin.sub(innerPlayerWidth / 2.0f, 0.0f);
+        Vector2f raycastEnd = new Vector2f(raycastBegin).add(0.0f, height);
+
+        RaycastInfo info = Window.getPhysics().raycast(gameObject, raycastBegin, raycastEnd);
+
+        Vector2f raycast2Begin = new Vector2f(raycastBegin).add(innerPlayerWidth, 0.0f);
+        Vector2f raycast2End = new Vector2f(raycastEnd).add(innerPlayerWidth, 0.0f);
+        RaycastInfo info2 = Window.getPhysics().raycast(gameObject, raycast2Begin, raycast2End);
+
+        DebugDraw.addLine2D(raycastBegin, raycastEnd, new Vector3f(1, 0, 0));
+        DebugDraw.addLine2D(raycast2Begin, raycast2End, new Vector3f(1, 0, 0));
+
+        return (info.hit && info.hitObject != null && info.hitObject.getComponent(Ground.class) != null) ||
+                (info2.hit && info2.hitObject != null && info2.hitObject.getComponent(Ground.class) != null);
     }
 }
